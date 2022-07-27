10a4d44cbd94e2d6654b43140540268159d62df8
indianazhao <indiana.zhao@gmail.com>
2022-07-27 22:02:49 +0800

    整合 @hirez_io/jest-given

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/src/setup.jest.ts'],
+  setupFilesAfterEnv: [
+    '<rootDir>/src/setup.jest.ts',
+    '<rootDir>/node_modules/@hirez_io/jest-given/dist/jest-given.js',
+  ],
   globalSetup: 'jest-preset-angular/global-setup',
 };
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,33 @@
-import { AppComponent } from './app.component';
+
+function addTwo(num: number) {
+  return num + 2;
+}
 
 describe('AppComponent', () => {
-  let fixture: AppComponent;
+  let firstNum: number;
+  let actualResult: number;
 
-  beforeEach(() => {
-    fixture = new AppComponent();
+  // THIS IS EXACTLY LIKE A `beforeEach`
+  // It's where you setup your code / inputs
+  Given(() => {
+    firstNum = 1;
   });
 
-  it('should have a little shai-jest', () => {
-    expect(fixture.title).toEqual('shai-jest');
+  // THIS IS A SPECIAL TYPE OF `beforeEach`
+  // It's where you call the action under test
+  When(() => {
+    actualResult = addTwo(firstNum);
   });
-})
+
+  // THIS IS EXACTLY LIKE A `it()`
+  // It's where you expect the desired outcome
+  Then(() => {
+    expect(actualResult).toEqual(3);
+  });
+
+  // You can also add a message
+  Then('it should be equal to 3', () => {
+    expect(actualResult).toEqual(3);
+  });
+
+});
